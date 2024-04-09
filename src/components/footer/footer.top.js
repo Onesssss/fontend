a5f5f6250ed88a3e1202bdb2313db445313c795c
Ones <minhquang2002nt@gmail.com>
2024-04-09 15:09:56 +0700

    update

---
+++ src/components/footer/footer.top.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+const FooterTop = () => (
+    <div className="footer-top">
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-3">
+                    <div className="companyinfo">
+                        <h2><span>Hutech</span></h2>
+                    </div>
+                </div>
+                <div className="col-sm-6"></div>
+          
+                <div className="col-sm-3">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+)
+export default FooterTop
